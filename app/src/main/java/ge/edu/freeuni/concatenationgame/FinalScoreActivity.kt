e9ae9f4f875850b6a1511f51af89e2e63c1dd86f
tkech17 <tornike.kechakmadze@azry.com>
2020-03-30 03:50:37 +0400

    added exit button

--- app/src/main/java/ge/edu/freeuni/concatenationgame/FinalScoreActivity.kt
+++ app/src/main/java/ge/edu/freeuni/concatenationgame/FinalScoreActivity.kt
@@ -6,21 +6,29 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.system.exitProcess
 
 
 class FinalScoreActivity : AppCompatActivity() {
 
     private lateinit var resultScoreTextView: TextView
     private lateinit var playAgainButton: Button
+    private lateinit var exitButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_final_score)
         playAgainButton = findViewById(R.id.playAgainButton)
         resultScoreTextView = findViewById(R.id.result_score_text_view)
+        exitButton = findViewById(R.id.exit_button)
 
         displayResultMessage()
         playAgainButton.setOnClickListener { startActivity(Intent(this, GameActivity::class.java)) }
+        exitButton.setOnClickListener {
+            moveTaskToBack(true)
+            android.os.Process.killProcess(android.os.Process.myPid());
+            exitProcess(1)
+        }
     }
 
     @SuppressLint("SetTextI18n")
